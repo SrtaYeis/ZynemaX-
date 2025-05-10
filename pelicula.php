3ba3ab3a28e3b06771d7606c80d7893033e07e59
SrtaYeis <jasminurrutia30@gmail.com>
2025-05-10 00:41:14 -0500

    Update pelicula.php

--- pelicula.php
+++ pelicula.php
@@ -6,7 +6,7 @@ session_start();
 // Conexión a la base de datos
 $serverName = "database-zynemaxplus-server.database.windows.net";
 $connectionInfo = [
-    "Database" => "ZynemaxDB",
+    "Database" => "database-zynemaxplus-server",
     "UID" => "zynemaxplus",
     "PWD" => "grupo2_1al10",
     "Encrypt" => true,
@@ -175,7 +175,7 @@ if (isset($_POST['process_payment'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zynemax+ | Películas</title>
-    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
     <header>
@@ -317,7 +317,7 @@ if (isset($_POST['process_payment'])) {
     <footer>
         <p>© 2025 Zynemax+ | Todos los derechos reservados</p>
     </footer>
-    <script src="/script.js" defer></script>
+    <script src="/scrip.js" defer></script>
     <?php sqlsrv_close($conn); ?>
 </body>
 </html>
