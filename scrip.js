aa96eb7158131cda5c3ebcde93accbf5e2b005f8
SrtaYeis <jasminurrutia30@gmail.com>
2025-05-09 23:02:33 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -3,11 +3,13 @@ function showForm(formType) {
     const loginForm = document.getElementById('login-form');
     const registerForm = document.getElementById('register-form');
     const profileForm = document.getElementById('profile-form');
+    const moviesForm = document.getElementById('movies-form');
 
     // Ocultar todos los formularios primero
     if (loginForm) loginForm.style.display = 'none';
     if (registerForm) registerForm.style.display = 'none';
     if (profileForm) profileForm.style.display = 'none';
+    if (moviesForm) moviesForm.style.display = 'none';
 
     // Mostrar el formulario correspondiente
     if (formType === 'login' && loginForm) {
@@ -16,5 +18,7 @@ function showForm(formType) {
         registerForm.style.display = 'block';
     } else if (formType === 'profile' && profileForm) {
         profileForm.style.display = 'block';
+    } else if (formType === 'movies' && moviesForm) {
+        moviesForm.style.display = 'block';
     }
 }
