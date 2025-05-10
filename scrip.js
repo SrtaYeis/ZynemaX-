f67ca3c4e8d56851fdbe04deb9d4f9ea6f3c97c8
SrtaYeis <jasminurrutia30@gmail.com>
2025-05-09 19:35:20 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -2,11 +2,11 @@ function showForm(formType) {
     const loginForm = document.getElementById('login-form');
     const registerForm = document.getElementById('register-form');
 
-    if (formType === 'login') {
-        loginForm.style.display = 'block';
-        registerForm.style.display = 'none';
-    } else if (formType === 'register') {
+    if (formType === 'register') {
         loginForm.style.display = 'none';
         registerForm.style.display = 'block';
+    } else {
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
     }
 }
