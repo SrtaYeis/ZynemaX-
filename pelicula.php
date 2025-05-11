388f59495f35c284569d51cf2d9829f72341e5e4
SrtaYeis <jasminurrutia30@gmail.com>
2025-05-10 21:49:35 -0500

    Update pelicula.php

--- pelicula.php
+++ pelicula.php
@@ -4,9 +4,9 @@ header("Content-Type: text/html; charset=UTF-8");
 session_start();
 
 // Depuración: Verificar el estado de la sesión
-echo "<pre>Estado de la sesión en pelicula.php:\n";
-var_dump($_SESSION);
-echo "</pre>";
+// echo "<pre>Estado de la sesión en pelicula.php:\n";
+// var_dump($_SESSION);
+// echo "</pre>";
 
 // Conexión a la base de datos
 $serverName = "database-zynemaxplus-server.database.windows.net";
